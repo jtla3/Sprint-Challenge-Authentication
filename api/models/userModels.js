c2f0e6963bc6f2dea991d9327adbc9d2774f7858
Mark Marchant <jtla3@yahoo.com>
2018-01-19 12:50:02 -0800

    Schema done

--- api/models/userModels.js
+++ api/models/userModels.js
@@ -1,8 +1,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+mongoose.models = {};
+mongoose.modelSchemas = {};
+
+mongoose.Promise = Promise;
+mongoose.connect('mongodb://localhost/users', { useMongoClient: true });
+
 const UserSchema = Schema({
   // create your user schema here.
+  username: {
+   type: String,
+   unique: true,
+   required: true,
+  },
+ password: {
+  type: String,
+ },
 });
 
 module.exports = mongoose.model('User', UserSchema);
