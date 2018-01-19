e1b102df467cd186a313c325e89ea4e6bd9ff816
Mark Marchant <jtla3@yahoo.com>
2018-01-19 15:02:54 -0800

    fixed error with hashed passwword

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -4,7 +4,8 @@ const bcrypt = require('bcrypt');
 const createUser = (req, res) => {
   // there should be a user object set on req
   // use that req.user object to create a user and save it to our Mongo instance.
-  const { username, password } = req.body
+  const { username } = req.body;
+  const { password } = req;
   const user = new User({ username, password });
   user.save((err,user) => {
     if (err) return res.send(err);
--- api/utils/middlewares.js
+++ api/utils/middlewares.js
@@ -22,14 +22,18 @@ const authenticate = (req, res, next) => {
 
 const encryptUserPW = (req, res, next) => {
   const { username, password } = req.body;
+  if (!password) {
+    sendUserError('Gimme a password', res);
+    return;
+  }
   // https://github.com/kelektiv/node.bcrypt.js#usage
   // TODO: Fill this middleware in with the Proper password encrypting, bcrypt.hash()
   // Once the password is encrypted using bcrypt you'll need to set a user obj on req.user with the encrypted PW
   // Once the user is set, call next and head back into the userController to save it to the DB
   bcrypt
    .hash(password, SaltRounds)
-   .then((pw) => {
-     req.password = pw;
+   .then(hash => {
+     req.password = hash;
      next();
    })
    .catch((err) => {
@@ -39,11 +43,21 @@ const encryptUserPW = (req, res, next) => {
 
 const compareUserPW = (req, res, next) => {
   const { username, password } = req.body;
+  User.findOne({ username })
   // https://github.com/kelektiv/node.bcrypt.js#usage
   // TODO: Fill this middleware in with the Proper password comparing, bcrypt.compare()
   // You'll need to find the user in your DB
   // Once you have the user, you'll need to pass the encrypted pw and the plaintext pw to the compare function
   // If the passwords match set the username on `req` ==> req.username = user.username; and call next();
+  bcrypt
+   .compare(password, hash)
+   .then(hash => {
+     res.hash = hash;
+   next();
+   })
+   .catch((err) => {
+     throw new Error(err);
+   });
 };
 
 module.exports = {
